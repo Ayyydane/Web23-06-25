7afda1e0be60eb82fc160e55d5f036e53f5b2478
Ayyydane <rabaydane@gmail.com>
2025-06-23 12:12:00 +0300

    connex

--- includes/connexion.php
+++ includes/connexion.php
@@ -5,3 +5,11 @@ if (!$bdd) {
     echo 'Erreur de connexion a la base de donnees';
 }
 ?>
+
+<?php
+$bdd = mysqli_connect('localhost', 'root', '', 'employees');
+
+if (!$bdd) {
+    echo 'Erreur de connexion a la base de donnees';
+}
+?>
